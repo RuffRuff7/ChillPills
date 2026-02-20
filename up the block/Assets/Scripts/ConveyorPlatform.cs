19eabac6a6d20061a8342e7925a9c6e213a0910b
naveedah-alt <naveedahadegoke@gmail.com>
2026-02-19 22:42:39 -0500

    tried to make spiral work, failed

--- up the block/Assets/Scripts/ConveyorPlatform.cs
+++ up the block/Assets/Scripts/ConveyorPlatform.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class ConveyorPlatform : MonoBehaviour
 {
     Vector3 direction;
+    float middlex;
+    MeshFilter meshfilter;
+    Mesh mesh;
     // Start is called before the first frame update
     
     void Start()
     {
         direction = transform.forward;
+        meshfilter = gameObject.GetComponent<MeshFilter>();
+        mesh = meshfilter.mesh;
+        middlex = (mesh.bounds.size.x * transform.localScale.x)/2.0f;
     }
 
     public Vector3 getDirection()
@@ -17,9 +23,14 @@ public class ConveyorPlatform : MonoBehaviour
         return direction;
     }
 
+    public float getMiddleX()
+    {
+        return middlex;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(middlex);
     }
 }
--- up the block/Assets/Scripts/Player.cs
+++ up the block/Assets/Scripts/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
 
     float yDirection;
 
+    float xPos;
+
     void Start()
     {
         //Fetch the Rigidbody component you attach from your GameObject
